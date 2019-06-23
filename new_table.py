2efe76d39ce4d5cfe84708ebf30013f7d8226c58
Alex Fedotov <alxfed@gmail.com>
2019-06-23 09:00:48 -0500

    working table creation

--- new_table.py
+++ new_table.py
@@ -12,7 +12,11 @@ table_service = TableService(account_name='lakemichigan',
 
 table_service.create_table('tasktable')
 
-task = {'PartitionKey': 'datatasks', 'RowKey': '001', 'description' : 'add 246 lines', 'priority' : 200}
-table_service.insert_entity('tasktable', task)
+point = {'PartitionKey': 'datapoints', 'RowKey': '001', 'description' : '246 lines', 'priority' : 200}
+table_service.insert_or_replace_entity('tasktable', point)
+
+"""
+insert_entity, insert_or_replace_entity, update_entity, merge_entity
+"""
 
 print('ok')
